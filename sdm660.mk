1ae63a462938c21603269ccc1f6b474963a1554f
predator112 <mi560334@gmail.com>
2021-05-01 08:39:11 +0000

    fix missing

--- sdm660.mk
+++ sdm660.mk
@@ -304,6 +304,10 @@ PRODUCT_PACKAGES += \
 
 
 # Media
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail_vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
