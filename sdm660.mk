14a8a543c4279b36db977be0e95f4249752dac7c
DillerOFire <niktofe1@gmail.com>
2021-05-04 07:13:31 +1000

    sdm660-common: Build power stats hal

--- sdm660.mk
+++ sdm660.mk
@@ -341,6 +341,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr \
+    android.hardware.power.stats@1.0-service.sdm660
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
