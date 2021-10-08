27af2d9ef5edd8d489f8740b83d20ce771670474
ini23 <92011891+ini23@users.noreply.github.com>
2021-10-08 08:12:47 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,13 @@
 # rm -rf 
-rm -rf packages/apps/Gallery2
-rm -rf hardware/qcom-caf/msm8998/display
-rm -rf hardware/qcom-caf/msm8998/media
-rm -rf hardware/qcom-caf/msm8998/audio
-rm -rf frameworks/base
-rm -rf packages/apps/Settings
-rm -rf frameworks/av
+#rm -rf packages/apps/Gallery2
+#rm -rf hardware/qcom-caf/msm8998/display
+#rm -rf hardware/qcom-caf/msm8998/media
+#rm -rf hardware/qcom-caf/msm8998/audio
+#rm -rf frameworks/base
+#rm -rf packages/apps/Settings
+#rm -rf frameworks/av
 #rm -rf kernel/xiaomi/sdm660
-rm -rf packages/services/Telecomm
+#rm -rf packages/services/Telecomm
 
 # git clone
 git clone https://github.com/NusantaraProject-ROM/android_packages_apps_Gallery2 -b 11 packages/apps/Gallery2
